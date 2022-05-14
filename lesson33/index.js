8233550da345abe23430473b8c4042fd93b5cafc
Lizaveta <liz.jane.v@gmail.com>
2022-05-14 09:19:42 +0300

    fix sortByTeachers

--- lesson33/index.js
+++ lesson33/index.js
@@ -1,88 +1,88 @@
 const subjects = {
-    mathematics: {
-        students: 200,
-        teachers: 6
-    },
-    biology: {
-        students: 120,
-        teachers: 6
-    },
-    geography: {
-        students: 60,
-        teachers: 2
-    },
-    chemistry: {
-        students: 100,
-        teachers: 3
-    }
-}
+  mathematics: {
+    students: 200,
+    teachers: 6,
+  },
+  biology: {
+    students: 120,
+    teachers: 6,
+  },
+  geography: {
+    students: 60,
+    teachers: 2,
+  },
+  chemistry: {
+    students: 100,
+    teachers: 3,
+  },
+};
 
 // 1
 function makeString(obj) {
-    const keys = Object.keys(obj)
-    const newString = keys.join(', ')
-    console.log(`All subjects: ${newString}`)
+  const keys = Object.keys(obj);
+  const newString = keys.join(", ");
+  console.log(`All subjects: ${newString}`);
 }
 //  2
 function countStudents(obj) {
-    const arr = Object.keys(obj)
-    let res = 0
-    arr.forEach(key => {
-        let value = obj[key].students
-        res += value
-    })
-    console.log(`Total students: ${res}`)
+  const arr = Object.keys(obj);
+  let res = 0;
+  arr.forEach((key) => {
+    let value = obj[key].students;
+    res += value;
+  });
+  console.log(`Total students: ${res}`);
 }
 
 function countTeachers(obj) {
-    const arr = Object.keys(obj)
-    let res = 0
-    arr.forEach(key => {
-        let value = obj[key].teachers
-        res += value
-    })
-    console.log(`Total teachers: ${res}`)
+  const arr = Object.keys(obj);
+  let res = 0;
+  arr.forEach((key) => {
+    let value = obj[key].teachers;
+    res += value;
+  });
+  console.log(`Total teachers: ${res}`);
 }
 
 // 3
 function averageStudents(obj) {
-    const arr = Object.keys(obj)
-    let res = 0
-    arr.forEach(key => {
-        let value = obj[key].students
-        res += value
-    })
+  const arr = Object.keys(obj);
+  let res = 0;
+  arr.forEach((key) => {
+    let value = obj[key].students;
+    res += value;
+  });
 
-    const keys = Object.keys(obj)
-    const general = keys.length
-    const average = res / general
-    console.log(`Average number of students: ${average}`)
+  const keys = Object.keys(obj);
+  const general = keys.length;
+  const average = res / general;
+  console.log(`Average number of students: ${average}`);
 }
 
 // 4
 function makeArr(obj) {
-    const val = Object.values(obj)
-    console.log(val)
+  const arr = Object.entries(obj).map(([subj, v]) => ({ subj, ...v }));
+  console.log(arr);
 }
 
 // 5
 function sortByTeachers(obj) {
-    const val = Object.values(obj)
+  const arr = Object.entries(obj).map(([subj, v]) => ({ subj, ...v }));
 
-    val.sort(function (a, b) {
-        return b.teachers - a.teachers
-    })
-    console.log(val)
+  arr.sort(function (a, b) {
+    return b.teachers - a.teachers;
+  });
+  console.log(arr);
 }
 
-makeString(subjects)
+makeString(subjects);
 
-countStudents(subjects)
+countStudents(subjects);
 
-countTeachers(subjects)
+countTeachers(subjects);
 
-averageStudents(subjects)
+averageStudents(subjects);
 
-makeArr(subjects)
+makeArr(subjects);
 
-sortByTeachers(subjects)
+sortByTeachers(subjects);
